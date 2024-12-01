9ff70ff982a40d384631da16a1b075ab38bd360e
itsbillw <itsbillw@gmail.com>
2024-12-01 20:46:38 +0100

    Day One - Second Star

--- day_01/day_01.py
+++ day_01/day_01.py
@@ -1,10 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv("day_one_input.txt", sep=" ", index_col=None, header=None, usecols=[0,3], names=["List1", "List2"])
+# read input into dataframe
+input_file = "day_one_input.txt"
+df = pd.read_csv(input_file, sep=" ", index_col=None, header=None, usecols=[0, 3], names=["List1", "List2"])
 
+# sort values in both columns in ascending order
 df['List1'] = df['List1'].sort_values().reset_index(drop=True)
 df['List2'] = df['List2'].sort_values().reset_index(drop=True)
 
+# calculate differences between lists
 df["Distance"] = abs(df["List1"] - df["List2"])
 
-print(df["Distance"].sum())
+print("Answer to part one is: ", df["Distance"].sum())
+
+# calculate frequency of value from first column in second column
+list2_counts = df['List2'].value_counts()
+df['Frequency'] = df['List1'] * df['List1'].map(list2_counts)
+
+print("Answer to part two is: ", df["Frequency"].sum())
